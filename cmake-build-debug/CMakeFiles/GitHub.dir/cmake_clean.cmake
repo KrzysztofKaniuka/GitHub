94a3e7e6948f7f9d75d78f55217e3706fd43df64
NightRaven <240697@edu.p.lodz.pl>
2022-01-13 19:53:50 +0100

    Nothing happened

---
+++ cmake-build-debug/CMakeFiles/GitHub.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GitHub.dir/main.c.obj"
+  "GitHub.exe"
+  "GitHub.exe.manifest"
+  "GitHub.pdb"
+  "libGitHub.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GitHub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/GitHub.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GitHub.
+# This may be replaced when dependencies are built.
